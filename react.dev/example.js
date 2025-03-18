6aa9bed78977f4d3d0225a06803201231c1d5bd3
Joe Ridyard <joeridyard3@gmail.com>
2025-03-17 20:07:29 -0500

    Example well written and documented with notes.txt completed

--- react.dev/example.js
+++ react.dev/example.js
@@ -10,22 +10,35 @@ const TERABITHIA_BRIDGE_ID = 'TERABITHIA_EXTENSION';
                 console.log('JSON response received from main context: ', response);
             });
 
-        (2) TerabithiaBridge[TERABITHIA_BRIDGE_ID].functions.helloFromIsolated({ message: 'Hello from isolated context!' }).then((response) => {
+        (2) const BRIDGE_FUNCTIONS = TerabithiaBridge[TERABITHIA_BRIDGE_ID].functions;
+
+            BRIDGE_FUNCTIONS.helloFromIsolated({ message: 'Hello from isolated context!' }).then((response) => {
                 console.log('JSON response received from main context: ', response);
             });
 
     * Anytime a handler is added to your bridge, it will become callable from the counterpart context via ".functions"
+
+    *** WARNING ***
+
+    YOU SHOULD ONLY USE "BRIDGE_FUNCTIONS" IF YOU ARE SURE THAT THE HANDLER HAS BEEN ADDED TO THE BRIDGE
+    IT TAKES A FEW MILLISECONDS ON SCRIPT LOAD FOR THE HANDLER TO BE ADDED TO THE BRIDGE AS A FUNCTION TO CALL DIRECTLY
+    IT IS SAFER TO WRITE THE WHOLE THING OUT LIKE IN VERSION (1) ABOVE
+
+    *** WARNING ***
 */
 
-const BRIDGE_FUNCTIONS = TerabithiaBridge[TERABITHIA_BRIDGE_ID].functions;
-
-console.log('BRIDGE_FUNCTIONS', BRIDGE_FUNCTIONS);
-console.log(TerabithiaBridge[TERABITHIA_BRIDGE_ID]);
-
-// BRIDGE_FUNCTIONS.getValueFromMain().then((response) => {
-//     console.log('JSON response received from main context: ', response);
-// });
+TerabithiaBridge[TERABITHIA_BRIDGE_ID].executeInMain('helloFromIsolated', {
+    message: 'Hello from isolated context!'
+}).then((response) => {
+    console.log('JSON response received from main context: ', response);
+});
 
 TerabithiaBridge[TERABITHIA_BRIDGE_ID].executeInMain('getValueFromMain').then((response) => {
     console.log('JSON response received from main context: ', response);
 });
+
+// bridge "functions" are only available after the bridge has been initialized fully, the below would not run since it runs alongside the scripts initialization process
+// const BRIDGE_FUNCTIONS = TerabithiaBridge[TERABITHIA_BRIDGE_ID].functions;
+// BRIDGE_FUNCTIONS.getValueFromMain().then((response) => {
+//     console.log('JSON response received from main context: ', response);
+// });
